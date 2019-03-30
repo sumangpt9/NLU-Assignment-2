c9b0a8d340cd8a36aa77c735c83baa87a7929be6
sumangpt9 <sumangpt9@gmail.com>
2019-03-31 04:47:50 +0530

    Update translation_german.py

--- translation_german.py
+++ translation_german.py
@@ -731,12 +731,8 @@ evaluate_randomly(encoder,decoder)
 # In[100]:
 
 
-
+##Testing
 from nltk.translate.bleu_score import sentence_bleu
-reference = [['this', 'is', 'a', 'test'], ['this', 'is' 'test']]
-candidate = ['this', 'is', 'a', 'test']
-score = sentence_bleu(reference, candidate)
-print(score)
 
 source_file = open("data/newstest2014-deen-ref.en.sgm", "r")
 target_file = open("data/newstest2014-deen-ref.de.sgm", "r")
